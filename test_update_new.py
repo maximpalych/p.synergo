ce24c1d471e6dd0c54d8bb6d36c27bf839a1a637
maximpalych <59122385+maximpalych@users.noreply.github.com>
2021-12-30 08:59:39 +0300

    Add files via upload

---
+++ test_update_new.py
@@ -0,0 +1,9 @@
+# SynergoMSVKTOOLS
+# version 1.0
+from update_check import isUpToDate
+VERSION = 2.0
+
+if __name__ == "__main__":
+
+    if isUpToDate(__file__, "https://raw.githubusercontent.com/maximpalych/p.synergo/main/test_update.py"):
+        print('new version found!')
