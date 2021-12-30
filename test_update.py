a6dcc0276f690a0a34279e1e36b922593513902c
maximpalych <59122385+maximpalych@users.noreply.github.com>
2021-12-30 08:58:55 +0300

    Delete test_update.py

--- test_update.py
+++
@@ -1,9 +0,0 @@
-# SynergoMSVKTOOLS
-# version 1.0
-from update_check import isUpToDate
-VERSION = 1.0
-
-if __name__ == "__main__":
-
-    if isUpToDate(__file__, "https://raw.githubusercontent.com/maximpalych/p.synergo/main/test_update.py"):
-        print('new version found!')
