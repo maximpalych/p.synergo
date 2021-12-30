e61cb2d188481861832de5a448d5731a9f555b5d
maximpalych <59122385+maximpalych@users.noreply.github.com>
2021-12-30 08:49:07 +0300

    Update test_update.py

--- test_update.py
+++ test_update.py
@@ -4,6 +4,5 @@ from update_check import isUpToDate
 VERSION = 2.0
 
 if __name__ == "__main__":
-
     if isUpToDate(__file__, "https://github.com/maximpalych/p.synergo/blob/main/test_update.py"):
         print('new version found!')
