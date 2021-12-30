c2870c6495532dbc2c68ae1d730647df4e14f53a
maximpalych <59122385+maximpalych@users.noreply.github.com>
2021-12-30 08:58:26 +0300

    Update test_update.py

--- test_update.py
+++ test_update.py
@@ -1,8 +1,9 @@
 # SynergoMSVKTOOLS
-# version 2.0
+# version 1.0
 from update_check import isUpToDate
-VERSION = 2.0
+VERSION = 1.0
 
 if __name__ == "__main__":
-    if isUpToDate(__file__, "https://github.com/maximpalych/p.synergo/blob/main/test_update.py"):
+
+    if isUpToDate(__file__, "https://raw.githubusercontent.com/maximpalych/p.synergo/main/test_update.py"):
         print('new version found!')
