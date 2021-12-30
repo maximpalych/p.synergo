7dad65a373a18bfd28e0bea64ab6b80077b5506c
maximpalych <59122385+maximpalych@users.noreply.github.com>
2021-12-30 08:48:56 +0300

    Update test_update.py

--- test_update.py
+++ test_update.py
@@ -6,5 +6,4 @@ VERSION = 2.0
 if __name__ == "__main__":
 
     if isUpToDate(__file__, "https://github.com/maximpalych/p.synergo/blob/main/test_update.py"):
-        print('new version not found!')
-        
+        print('new version found!')
