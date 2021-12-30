dfca0adcb45eac2d6d5b5691104227a8fb98f8ac
maximpalych <59122385+maximpalych@users.noreply.github.com>
2021-12-30 11:08:27 +0300

    Add files via upload

---
+++ test_update.py
@@ -0,0 +1,8 @@
+# SynergoMSVKTOOLS
+# version 1.0
+from update_check import checkForUpdates
+VERSION = 1.0
+RAW_PATH = 'https://raw.githubusercontent.com/maximpalych/p.synergo/main/test_update.py'
+
+if __name__ == "__main__":
+    checkForUpdates(__file__, RAW_PATH)
